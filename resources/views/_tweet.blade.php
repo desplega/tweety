19218b72802f430be98410be2c431fbebb958e9e
desplega <desplega@desplega.com>
2021-04-18 19:23:45 +0200

    Chapter 56 Design the Timeline

---
+++ resources/views/_tweet.blade.php
@@ -0,0 +1,11 @@
+<div class="flex p-4 border-b border-b-gray-400">
+    <div class="mr-2 flex-shrink-0">
+        <img src="https://i.pravatar.cc/50" alt="" class="rounded-full mr-2">
+    </div>
+    <div>
+        <h5 class="font-bold mb-4">John Doe</h5>
+        <p class="text-sm">This is the text of the tweet that will be shown in the text section. This is the
+            text of the tweet that will be shown in the text section. This is the text of the tweet that
+            will be shown in the text section.</p>
+    </div>
+</div>
